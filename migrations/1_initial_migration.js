f70cd82e0ee5c1f64593d6fbf7242fbb1f4f4ef1
TepNik <tepelin.nikita@gmail.com>
2020-11-06 19:03:32 +0300

    Changed migration file

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,17 +2,24 @@ const Praem = artifacts.require("Praem");
 
 const BN = require("bn.js");
 
-//0x7A2D87F5DB7B0825264CC6329D95FD660952DEc7
 const owner = "0x7A2D87F5DB7B0825264CC6329D95FD660952DEc7";
-// 0
-// 1610236800 -- Sun, 10 Jan 2021 00:00:00 GMT
-const openTime = new BN(1610236800);
+const eth1 = "0x4669E5197f0956Ee210dE41DD67941004Ae1e534";
+const eth2 = "0x3FC824a60DFd4824439713d62e3B82F35ec42fE9";
+const PraemtoOwner = new BN(10 * (10 ** 6)).mul(new BN(10 ** 8));
+const PraemtoEth1 = new BN(6 * (10 ** 6)).mul(new BN(10 ** 8));
+const PraemtoEth2 = new BN(4 * (10 ** 6)).mul(new BN(10 ** 8));
+
+// 1604678400 -- 11/06/2020 @ 4:00pm (UTC)
+const openTime = new BN(1604678400);
 
 module.exports = async function (deployer) {
     let PraemInst;
     await deployer.deploy(Praem, openTime);
     PraemInst = await Praem.deployed();
 
-    await PraemInst.transfer(owner, await PraemInst.totalSupply());
+    await PraemInst.transfer(owner, PraemtoOwner);
+    await PraemInst.transfer(eth1, PraemtoEth1);
+    await PraemInst.transfer(eth2, PraemtoEth2);
+    await PraemInst.addWhiteListTransfer(eth2);
     await PraemInst.transferOwnership(owner);
 };
